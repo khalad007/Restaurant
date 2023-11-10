e9beac494d2d9614cda3ea40f20259518582bf9e
khalad007 <www.arifhossain49919@gmail.com>
2023-11-10 06:53:08 -0800

    Setup routes,layout,footer and navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='hs'>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   </React.StrictMode>,
 )
