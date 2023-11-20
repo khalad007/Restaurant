a5c745b2035bc34cbcd982785287fe876c5669c9
khalad007 <www.arifhossain49919@gmail.com>
2023-11-20 04:41:27 -0800

    Install stripe and create card element

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe('');
 
 const Payment = () => {
     return (
         <div>
            <SectionTitle subHeading="Payment" heading="---Make me Rich---"></SectionTitle> 
+
+           <div>
+            <Elements stripe={stripePromise}>
+            
+            </Elements>
+           </div>
         </div>
     );
 };
